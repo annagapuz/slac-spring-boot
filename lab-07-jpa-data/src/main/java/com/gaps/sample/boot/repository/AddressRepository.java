b3d367c573f96e706e76ca1d82abb224553d6111
Anna Gapuz <amgapuz@gmail.com>
2019-02-26 00:02:00 -0500

    Blanked out for lab work

--- lab-07-jpa-data/src/main/java/com/gaps/sample/boot/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gaps.sample.boot.repository;
-
-import com.gaps.sample.boot.entity.Address;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AddressRepository extends CrudRepository<Address, Long> {
-
-    Address findFirstByCityStartsWith(String startingLetter);
-}
